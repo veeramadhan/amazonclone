d63afff24ddd15b233e94b14ecfeb6e64cc4360a
veeramadhan <95845583+veeramadhan@users.noreply.github.com>
2024-05-04 14:25:18 +0530

    second  commit

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -120,7 +120,7 @@ const Nav = () => {
 
 
 <div className="carouselMain">
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleInterval" class="" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
       <img src={banner3} class="d-block ban" alt="..."/>
@@ -142,11 +142,13 @@ const Nav = () => {
   </button>
 </div>
 
-</div>
-
 <div className="headerMain">
 <Header />
 </div>
+
+</div>
+
+
 
 
 
